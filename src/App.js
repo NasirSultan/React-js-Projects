68700729ea932e21db6e69c537d8be6afd42bd1c
NasirSultan <rainasirsultan123@gmail.com>
2024-11-24 01:27:45 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import ProductCard from './ProductCard';
-import './App.css';
-import image1 from './download (1).jpg';
-import image2 from './download (2).jpg';
-import image3 from './images.jpg';
-import image4 from './download (3).jpg';
+import React from "react";
 
-const products = [
-  { name: 'Computer', price: 29.99, image: image1 },
-  { name: 'Mouse', price: 49.99, image: image2 },
-  { name: 'Keyboard', price: 19.99, image: image3 },
-  { name: 'Laptop', price: 99.99, image: image4 },
-];
-
-const App = () => {
+function App() {
   return (
-    <div className="app">
-      <Header />
-      <div className="product-list">
-        {products.map((product, index) => (
-          <ProductCard key={index} {...product} />
-        ))}
-      </div>
-      <Footer />
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my first React app.</p>
     </div>
   );
-};
+}
 
 export default App;
